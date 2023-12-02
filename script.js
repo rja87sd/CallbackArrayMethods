f8d42e08b3b85aec38468a98f7d0fa6f7657b042
Ryan Arnold <rja87.sd@gmail.com>
2023-12-02 17:53:42 -0600

    Create h1 in index.html. Add content for first step in script.js.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+// Declare global variables.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Use the map method with a callback function to modify each element (e.g., multiply by 2) and log the new array.
+const byTwo = numbers.map((eachNumber) => {
+  const productNumber = eachNumber * 2;
+  return `${eachNumber} x 2 is ${productNumber}`;
+});
+console.log(byTwo);
+
+
+
